7a9eb0dedb1ae7d3581cb704211474fda610902f
SoCretino <gonzalospota@gmail.com>
2018-03-14 00:13:07 +0000

    Minor modification to meta-tag description

--- item.php
+++ item.php
@@ -25,8 +25,8 @@
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-		<meta name="description" content="<?php echo $description ?>">
-		<meta itemprop="description" content="<?php echo $description ?>">
+		<meta name="description" content='<?php echo $description ?>'>
+		<meta itemprop="description" content='<?php echo $description ?>'>
 		<meta itemprop="name" content="BISPO">
 		<meta itemprop="image" content="http://bispo.info/img/logo_social.png">
 		<meta property="og:type" content="website">
